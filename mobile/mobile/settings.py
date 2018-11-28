c48584fc1c0614bb99583718ecd59a818d4b0f31
Nandinski <nando_14sclm@hotmail.com>
2018-11-28 18:22:13 +0000

    small interface changes

--- mobile/mobile/settings.py
+++ mobile/mobile/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mobile_app.apps.MobileAppConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
